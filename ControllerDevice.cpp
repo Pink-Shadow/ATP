e12ce67cb51a78ee489e08e1af1289abeeaaf8d1
Jasper <jaspervdpost@gmail.com>
2023-03-14 13:21:37 +0100

    Added support for ControllerDevice class
    modifield makefile
    added new serial library
    added serial library as submodule

---
+++ ControllerDevice.cpp
@@ -0,0 +1,19 @@
+#include "ControllerDevice.hpp"
+
+ControllerDevice::ControllerDevice(serial::Serial &USB_dev):
+    USB_Device(USB_dev)
+{}
+
+void ControllerDevice::set_cruise_state(bool state){
+    cruise_state = state;
+}
+
+void ControllerDevice::set_brake(bool brake){
+    // TODO: add function call to Controller_input entitiy "Controller_input.change_brakes(brake)"
+    std::cout << "brake changed to: " << brake << std::endl;
+}
+
+int ControllerDevice::read_acceleration(){
+    return speed_throttle; // maybe, later add class function call speed_throttle.read()
+}
+
---
+++ ControllerDevice.hpp
@@ -0,0 +1,17 @@
+
+#include "serial/serial.h"
+#include <iostream>
+
+class ControllerDevice {
+private:
+    serial::Serial &USB_Device;
+    bool cruise_state;
+    int speed_throttle; // TODO: make class? to read out value.
+
+public:
+    ControllerDevice(serial::Serial &USB_dev);
+
+    void set_cruise_state(bool state);
+    void set_brake(bool brake);
+    int read_acceleration();
+};
